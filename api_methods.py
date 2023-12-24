e92b6953e526f9d0fd59ed8d627a8f654fef2849
Kemal Soylu <kemalbsoylu@gmail.com>
2023-12-24 09:27:39 +0300

    updated docstrings of methods

--- api_methods.py
+++ api_methods.py
@@ -1,6 +1,19 @@
 import pandas as pd
 
 def get_channel_stats(youtube, channel_ids):
+    """
+    Get channel statistics: title, subscriber count, view count, video count, upload playlist
+
+    Params:
+
+    youtube: the build object from googleapiclient.discovery
+    channels_ids: list of channel IDs
+    
+    Returns:
+    Dataframe containing the channel statistics for all channels in the provided list: title, subscriber count, view count, video count, upload playlist
+    
+    """
+
     all_data = []
 
     request = youtube.channels().list(
@@ -24,6 +37,19 @@ def get_channel_stats(youtube, channel_ids):
 
 
 def get_video_ids(youtube, playlist_id):
+    """
+    Get list of video IDs of all videos in the given playlist
+    
+    Params:
+    
+    youtube: the build object from googleapiclient.discovery
+    playlist_id: playlist ID of the channel
+    
+    Returns:
+    List of video IDs of all videos in the playlist
+    
+    """
+
     video_ids = []
 
     request = youtube.playlistItems().list(
